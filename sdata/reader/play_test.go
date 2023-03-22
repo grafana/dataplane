7c71562068b23602f69a9e2a5e157a4de0096986
Kyle Brandt <kyle@grafana.com>
2023-03-22 08:10:18 -0400

    sdata/reader: Add test using examples extracted from https://github.com/grafana/dataplane/pull/5

--- sdata/reader/play_test.go
+++ sdata/reader/play_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/grafana/dataplane/examples"
 	"github.com/grafana/dataplane/sdata/numeric"
 	"github.com/grafana/dataplane/sdata/reader"
 	"github.com/grafana/dataplane/sdata/timeseries"
@@ -57,4 +58,69 @@ func TestCanReadBasedOnMeta(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestExamplesCollections(t *testing.T) {
+	examples, err := examples.GetExamples()
+	require.NoError(t, err)
+
+	collections := examples.Collections()
+	_ = collections
+}
+
+func TestCanReadTestData(t *testing.T) {
+	es, err := examples.GetExamples()
+	require.NoError(t, err)
+
+	numericV01Examples, err := es.Filter(examples.FilterOptions{
+		Kind:    data.KindNumeric,
+		Version: data.FrameTypeVersion{0, 1},
+	})
+	require.NoError(t, err)
+
+	for _, collection := range numericV01Examples.Collections() {
+		for _, example := range collection.ExampleSlice() {
+			t.Run(example.Info().ID, func(t *testing.T) {
+				kind, err := reader.CanReadBasedOnMeta(example.Frames("A"))
+				require.NoError(t, err)
+				require.Equal(t, example.Info().Type.Kind(), kind)
+				require.Equal(t, data.KindNumeric, kind)
+
+				nr, err := numeric.CollectionReaderFromFrames(example.Frames("A"))
+				require.NoError(t, err)
+
+				c, err := nr.GetCollection(false)
+				require.NoError(t, err)
+				require.NoError(t, c.Warning)
+
+				require.Len(t, c.Refs, example.Info().ItemCount)
+			})
+		}
+	}
+
+	timeseriesV01Examples, err := es.Filter(examples.FilterOptions{
+		Kind:    data.KindTimeSeries,
+		Version: data.FrameTypeVersion{0, 1},
+	})
+	require.NoError(t, err)
+
+	for _, collection := range timeseriesV01Examples.Collections() {
+		for _, example := range collection.ExampleSlice() {
+			t.Run(example.Info().ID, func(t *testing.T) {
+				kind, err := reader.CanReadBasedOnMeta(example.Frames("A"))
+				require.NoError(t, err)
+				require.Equal(t, example.Info().Type.Kind(), kind)
+				require.Equal(t, data.KindTimeSeries, kind)
+
+				nr, err := timeseries.CollectionReaderFromFrames(example.Frames("A"))
+				require.NoError(t, err)
+
+				c, err := nr.GetCollection(false)
+				require.NoError(t, err)
+				require.NoError(t, c.Warning)
+
+				require.Len(t, c.Refs, example.Info().ItemCount)
+			})
+		}
+	}
 }
