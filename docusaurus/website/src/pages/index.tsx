ddc32a17c235028ad0b92bde5712404c082e37f9
Sriramajeyam Sugumaran <153843+yesoreyeram@users.noreply.github.com>
2023-05-11 15:11:23 +0100

    docusaurus website

---
+++ docusaurus/website/src/pages/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import GrafanaLogo from "./homepage_logo.svg";
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className={styles.heroImageContainer}>
+        <GrafanaLogo style={{ color: "rgba(255, 255, 255, 0.2)" }} />
+      </div>
+      <div className={clsx("container", styles.heroContent)}>
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg button--outline"
+            to="/contract"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`} description={siteConfig.tagline}>
+      <HomepageHeader />
+    </Layout>
+  );
+}
